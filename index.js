2c34abe269211d32898bc44d1e4903f80028311c
SmallPeenwow <bowdencamryn@gmail.com>
2023-04-28 11:24:12 +0200

    Open On New HTML Page Done

---
+++ index.js
@@ -0,0 +1,7 @@
+import SendToSignInPage from './src/landingPage.js';
+
+const SignIn = document.querySelector('#sign-in');
+
+SignIn.addEventListener('click', () => {
+	SendToSignInPage();
+});
---
+++ src/landingPage.js
@@ -0,0 +1,5 @@
+const SendToSignInPage = () => {
+	window.open('./pages/signIn.html', '_self');
+};
+
+export default SendToSignInPage;
