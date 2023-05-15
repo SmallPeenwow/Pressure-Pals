2cbfb420286c9a0cda015d2fb5242d2190d3af92
SmallPeenwow <bowdencamryn@gmail.com>
2023-05-15 18:25:28 +0200

    DropDown Month Done

---
+++ client/src/components/UserHome/dropDownList.js
@@ -0,0 +1,34 @@
+const monthList = document.querySelector('#month-list');
+
+window.onload = () => {
+	monthListUpdate();
+};
+
+const monthNames = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
+const monthListUpdate = () => {
+	for (let i = 0; i < monthNames.length; i++) {
+		const option = document.createElement('option');
+		option.innerText = monthNames[i];
+		option.value = monthNames[i];
+		monthList.appendChild(option);
+
+		if (i === new Date().getMonth()) {
+			console.log(new Date().getMonth().toString());
+			option.selected = true;
+		}
+	}
+};
